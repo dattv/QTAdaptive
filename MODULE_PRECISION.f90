01cfc9cbb3deb85eefb2cad5b1104e2967f36270
dattv <datthangvan@gmail.com>
2018-07-11 20:17:38 +0900

    clearn module_precision

--- MODULE_PRECISION.f90
+++ MODULE_PRECISION.f90
@@ -11,11 +11,11 @@ MODULE MODULE_PRECISION
     
     public  :: ip, rp
     
-    integer, parameter  :: sp  = kind(1.d0)
-    integer, parameter  :: p2  = selected_real_kind(2*precision(1._sp))
-    integer, parameter  :: i5  = selected_int_kind(5)
+    integer, parameter  ::  sp = kind(1.d0)
+    integer, parameter  ::  p2 = selected_real_kind(2*precision(1._sp))
+    integer, parameter  ::  i5 = selected_int_kind(5)
     integer, parameter  :: i15 = selected_int_kind(15)
-    integer, parameter  :: ip  = i5
-    integer, parameter  :: rp  = p2
+    integer, parameter  ::  ip = i5
+    integer, parameter  ::  rp = p2
     
 END MODULE MODULE_PRECISION    
