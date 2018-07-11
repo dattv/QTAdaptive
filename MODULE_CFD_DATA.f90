5fc6795e59e3ef756582041bc07aeb7e2074f211
dattv <datthangvan@gmail.com>
2018-07-11 19:33:31 +0900

    update method for cfd_data module

--- MODULE_CFD_DATA.f90
+++ MODULE_CFD_DATA.f90
@@ -20,9 +20,15 @@
         	real(rp)                            :: wsn      ! wave speed
             
         contains
-        procedure   :: new => new_cfd_data
+        procedure   ::    new => new_cfd_data
         procedure   :: delete => delete_cfd_data
+        
         end type cfd_data
+
+!=================== INTERFACE =====================
+        interface assignment(=)
+            module procedure asign_cfd
+        end interface 
         
     contains
 !================================================================================================= 
@@ -91,10 +97,31 @@
     implicit none
     class(cfd_data), intent(inout)  :: this
     
-    ! body 
+    ! body
+    if (allocated(this%u    )) deallocate(this%u    )  ! conservative variables
+    if (allocated(this%w    )) deallocate(this%w    )  ! primative variables
+    if (allocated(this%u_old)) deallocate(this%u_old)  ! conservative variables
+    if (allocated(this%u_new)) deallocate(this%u_new)  ! conservative variables
+    if (allocated(this%res  )) deallocate(this%res  )  ! residual    
     return
     end subroutine delete_cfd_data
-!=================================================================================================  
+!=================================================================================================
+    subroutine asign_cfd(out_cfd_data, in_cfd_data)
+    implicit none
+    type(cfd_data), intent(out)  :: out_cfd_data
+    type(cfd_data), intent(in)   :: in_cfd_data
+    
+    call out_cfd_data%new(in_cfd_data%NQ)
+    
+    out_cfd_data%NQ     = in_cfd_data%NQ       ! total equations
+    out_cfd_data%u      = in_cfd_data%u        ! conservative variables
+    out_cfd_data%w      = in_cfd_data%w        ! primative variables
+    out_cfd_data%u_old  = in_cfd_data%u_old    ! conservative variables
+    out_cfd_data%u_new  = in_cfd_data%u_new    ! conservative variables
+    out_cfd_data%res    = in_cfd_data%res      ! residual
+    out_cfd_data%wsn    = in_cfd_data%wsn      ! wave speed
+    return
+    end subroutine  asign_cfd
 !=================================================================================================  
 !=================================================================================================  
 !=================================================================================================  
